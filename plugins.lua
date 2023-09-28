6aaa5af875ebdd3c10774f203bf48fa0ef976c0c
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2023-09-28 02:27:38 -0300

    Update plugins.lua

--- plugins.lua
+++ plugins.lua
@@ -5,16 +5,9 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "rust-analyzer",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
+        "rust-analyzer"
+      }
+    }
   },
   {
     "simrat39/rust-tools.nvim",
@@ -28,23 +21,17 @@ local plugins = {
     end
   },
   {
-    "mfussenegger/nvim-dap",
-    init = function()
-      require("core.utils").load_mappings("dap")
-    end
-  },
-  {
     'saecki/crates.nvim',
     ft = {"toml"},
     config = function(_, opts)
-      local crates  = require('crates')
+      local crates = require('crates')
       crates.setup(opts)
-      require('cmp').setup.buffer({
+      cmp.setup.buffer({
         sources = { { name = "crates" }}
       })
       crates.show()
       require("core.utils").load_mappings("crates")
-    end,
+    end
   },
   {
     "rust-lang/rust.vim",
@@ -52,26 +39,6 @@ local plugins = {
     init = function ()
       vim.g.rustfmt_autosave = 1
     end
-  },
-  {
-    "theHamsta/nvim-dap-virtual-text",
-    lazy = false,
-    config = function(_, opts)
-      require("nvim-dap-virtual-text").setup()
-    end
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local M = require "plugins.configs.cmp"
-      M.completion.completeopt = "menu,menuone,noselect"
-      M.mapping["<CR>"] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = false,
-      }
-      table.insert(M.sources, {name = "crates"})
-      return M
-    end,
   }
 }
 return plugins
